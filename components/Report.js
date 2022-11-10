03d7e7d4e3b9f17515eae08d9452179ca48e8c08
Junio Almeida <junio.almeida@rossetti.com.br>
2022-11-10 12:20:39 -0300

    Fix report retrieve of items to avoid get null object

--- components/Report.js
+++ components/Report.js
@@ -12,12 +12,14 @@ export default function Report() {
         let totalProducts = 0;
         let totalValue = 0;
         const rows = [];
+        const categories = context.categories ? context.categories : [];
+        const products = context.products ? context.products : [];
 
-        context.categories.forEach(c => {
+        categories.forEach(c => {
             let totalStored = 0;
             let totalPrice = 0;
 
-            context.products.map(p => {
+            products.map(p => {
                 if (p.codCategory === c.id) {
                     totalPrice += p.stored * p.price;
                     totalStored += p.stored;
